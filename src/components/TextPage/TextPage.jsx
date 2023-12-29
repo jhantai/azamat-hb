5a938e3c334ee3118270fd16ee8b5a4402efc7eb
unknown <tylerbroceva123@gmail.com>
2023-12-29 16:21:10 +0600

    first commit

--- src/components/TextPage/TextPage.jsx
+++ src/components/TextPage/TextPage.jsx
@@ -1,4 +1,4 @@
-import Carousel from 'react-bootstrap/Carousel';
+
 import azamat1 from "../images/azamat1.jpeg";
 import azamat2 from "../images/azamat2.jpeg";
 import azamat8 from "../images/azamat8.jpg";
@@ -6,37 +6,22 @@ import razdelitel2 from "../images/razdelitel2.png"
 import './TextPage.css'
 
 function TextPage() {
-    return (
+  return (
     <div>
-        <img className='razdelitel1' src={razdelitel2} alt="" />
+      <div className=" homePage">
+      <img className='razdelitel1' src={razdelitel2} alt="" />
       <h1>C Днём Рождения Азамат </h1>
-      <Carousel.Item>
       <img src={azamat1} alt="" />
-      
-        <Carousel.Caption>
-          <h3></h3>
-          <h1 className='pozhelanie'>Уважаемый Азамат Рыспаевич,извольте поздравить вас с днем рождения</h1>
-          <img className='razdelitel' src={razdelitel2} alt="" />
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
+      <h1 className='pozhelanie'>Уважаемый Азамат Рыспаевич,извольте поздравить вас с днем рождения</h1>
+      <img className='razdelitel' src={razdelitel2} alt="" />
       <img src={azamat2} alt="" />
-        <Carousel.Caption>
-          <h1></h1>
-          <h1></h1>
-          <h1 className='pozhelanie-1'>пожелать вам,чтобы ваш дух никогда не истощался , <br />будьте всегда блестящим примером для подражания</h1>
-          <img className='razdelitel' src={razdelitel2} alt="" />
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
+      <h1 className='pozhelanie-1'>пожелать вам,чтобы ваш дух никогда не истощался , <br />будьте всегда блестящим примером для подражания</h1>
+      <img className='razdelitel' src={razdelitel2} alt="" />
       <img src={azamat8} alt="" />
-        <Carousel.Caption>
-          <h1> Ловите каждый счастливый момент в вашей жизни и наслаждайтесь им .</h1>
-        
-          <img className='razdelitel' src={razdelitel2} alt="" />
-        </Carousel.Caption>
-      </Carousel.Item>
+      <h1> Ловите каждый счастливый момент в вашей жизни и наслаждайтесь им .</h1>
+      <img className='razdelitel' src={razdelitel2} alt="" />
       </div>
+    </div>
   );
 }
 
