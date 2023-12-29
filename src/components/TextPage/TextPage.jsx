d3b881f662a0b3aa0e5543970681027dc15efdaa
unknown <tylerbroceva123@gmail.com>
2023-12-29 16:52:52 +0600

    fourth commit

--- src/components/TextPage/TextPage.jsx
+++ src/components/TextPage/TextPage.jsx
@@ -1,7 +1,7 @@
 
-import azamat1 from "../images/azamat1.jpeg";
-import azamat2 from "../images/azamat2.jpeg";
-import azamat8 from "../images/azamat8.jpg";
+import azamat11 from "../images/azamat11.jpg";
+import azamat22 from "../images/azamat22.jpg";
+import azamat88 from "../images/azamat88.jpg";
 import razdelitel2 from "../images/razdelitel2.png"
 import './TextPage.css'
 
@@ -11,13 +11,13 @@ function TextPage() {
       <div className=" homePage">
       <img className='razdelitel1' src={razdelitel2} alt="" />
       <h1>C Днём Рождения Азамат </h1>
-      <img src={azamat1} alt="" />
+      <img src={azamat11} alt="" />
       <h1 className='pozhelanie'>Уважаемый Азамат Рыспаевич,извольте поздравить вас с днем рождения</h1>
       <img className='razdelitel' src={razdelitel2} alt="" />
-      <img src={azamat2} alt="" />
+      <img src={azamat22} alt="" />
       <h1 className='pozhelanie-1'>пожелать вам,чтобы ваш дух никогда не истощался , <br />будьте всегда блестящим примером для подражания</h1>
       <img className='razdelitel' src={razdelitel2} alt="" />
-      <img src={azamat8} alt="" />
+      <img src={azamat88} alt="" />
       <h1> Ловите каждый счастливый момент в вашей жизни и наслаждайтесь им .</h1>
       <img className='razdelitel' src={razdelitel2} alt="" />
       
